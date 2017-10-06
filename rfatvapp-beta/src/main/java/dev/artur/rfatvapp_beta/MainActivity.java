10aff459704b8a80d49a79435283d91809ca06eb
Artur de Araujo <artur.at4@gmail.com>
2017-10-06 15:06:21 -0300

    Adicionado um novo módulo de teste para experimentar a abordagem de conexão por bluetooth antiga.
    ## TODO:
    ### Implementar tela de leitura de EPC.

---
+++ rfatvapp-beta/src/main/java/dev/artur/rfatvapp_beta/MainActivity.java
@@ -0,0 +1,28 @@
+package dev.artur.rfatvapp_beta;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
